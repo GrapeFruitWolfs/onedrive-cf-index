391171ce3ba09e4d801eb6aecb8f3dd9233d2d47
GrapeFruitWolf <38470370+GrapeFruitWolfs@users.noreply.github.com>
2021-07-12 05:50:44 +0900

    Update default.js

--- src/config/default.js
+++ src/config/default.js
@@ -18,7 +18,7 @@ const config = {
   },
 
   refresh_token: REFRESH_TOKEN,
-  client_id: '6600e358-9328-4050-af82-0af9cdde796b',
+  client_id: '1b973335-5c01-4a1f-9002-e4125725854b',
   client_secret: CLIENT_SECRET,
 
   /**
